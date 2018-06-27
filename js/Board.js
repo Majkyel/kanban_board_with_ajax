6f94c280d10ef2a3fdfced5e8facabebd2b6c13f
Michał Kuliś <michalkulis1993@gmail.com>
2018-06-28 00:29:50 +0200

    Update .js files

--- js/Board.js
+++ js/Board.js
@@ -1,5 +1,5 @@
 var board = {
-    name: 'Tablica Kanban',
+    name: 'Kanban board',
     addColumn: function (column) {
         this.element.appendChild(column.element);
         initSortable(column.id); 
@@ -9,8 +9,16 @@ var board = {
 
 document.querySelector('#board .create-column').addEventListener('click', function () {
     var name = prompt('Enter a column name');
-    var column = new Column(name);
-    board.addColumn(column);
+    var data = new FormData();
+    data.append('name', name);
+    fetch(baseUrl + '/column', {method: 'POST', headers: myHeaders, body: data,})
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(resp){
+        var column = new Column(resp.id, name);
+        board.addColumn(column);
+    });
 });
 
 function initSortable(id) {
